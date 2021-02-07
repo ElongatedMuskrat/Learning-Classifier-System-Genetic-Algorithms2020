3774a1021d9998d831cbc6a21f923cfbbfdfd394
ElongatedMuskrat <ibkolendo@alaska.edu>
2021-02-06 19:59:24 -0900

    started fitness rating

--- lcs.py
+++ lcs.py
@@ -7,26 +7,27 @@ def fitness(short, long, binary):
     print("Candidate: ", candidate)
     place = 0
     match = 0
-    for y in range(len(candidate)):
+    numOfOnes = len(candidate)
+    for y in range(numOfOnes):
         for z in range(len(long)):
             if z == 0:
                 z = place
-            print("place", place, candidate[y],":::" ,long[z])
+            #print("place", place, candidate[y],":::" ,long[z])
             if candidate[y] == long[z]:
-                print("y = ", y, "z = ", z)
+                #print("y = ", y, "z = ", z)
                 place = z + 1
                 match += 1
-                print("Place: ", place, "z", z)
+                #print("Place: ", place, "z", z)
                 break
 
     print("Long: ", long)
     print("Match: ", match)
-
-
+    fitnessRating = match + (match * numOfOnes)
+    print("Fitness: ", fitnessRating)
 def main():
   print("Sup my nug")
-  fitness("popo", "poooooooooopoo", "1110")
-  #fitness("president", "providence", "110011110") #priden
+  #fitness("popo", "poooooooooopoo", "1110")
+  fitness("president", "providence", "110011110") #priden
   #fitness("president", "providence", "100000101") #pet
 
 if __name__ == '__main__':
