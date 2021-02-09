597752a86bb5cd5b3d9347312b61f1e193096074
ElongatedMuskrat <ibkolendo@alaska.edu>
2021-02-08 17:08:48 -0900

    fitness

--- lcs.py
+++ lcs.py
@@ -1,34 +1,42 @@
 
-def fitness(short, long, binary):
+def fitness(short, longo, binary):
     candidate = ""
     for x in range(len(binary)):
         if binary[x] == "1":
             candidate += (short[x])
-    print("Candidate: ", candidate)
+    #print("Candidate: ", candidate)
     place = 0
     match = 0
     numOfOnes = len(candidate)
+    z = 0
     for y in range(numOfOnes):
-        for z in range(len(long)):
-            if z == 0:
-                z = place
-            #print("place", place, candidate[y],":::" ,long[z])
-            if candidate[y] == long[z]:
-                #print("y = ", y, "z = ", z)
-                place = z + 1
-                match += 1
-                #print("Place: ", place, "z", z)
+        z = place
+        while z < len(longo):
+            if longo[z] == candidate[y]:
+                if place <= z:
+                    place = z + 1
+                    match += 1
                 break
+            z += 1
 
-    print("Long: ", long)
+
+    print("longo: ", longo)
     print("Match: ", match)
-    fitnessRating = match + (match * numOfOnes)
+    fitnessRating = match + (match * numOfOnes) - (numOfOnes - match)
     print("Fitness: ", fitnessRating)
 def main():
-  print("Sup my nug")
-  #fitness("popo", "poooooooooopoo", "1110")
+
+
+  print("##########################################")
   fitness("president", "providence", "110011110") #priden
-  #fitness("president", "providence", "100000101") #pet
+  print("##########################################")
+  fitness("president", "providence", "100000101") #pet
+  print("##########################################")
+  fitness("president", "providence", "001100001") #
+  print("##########################################")
+  fitness("sourapple", "googleappstore", "010010101") #
+  print("##########################################")
+  fitness("sourapple", "googleappstore", "111111111") #
 
 if __name__ == '__main__':
   main()
